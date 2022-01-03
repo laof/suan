65a27c874d16c620cce6625a2b82150ceac524fd
laof <345603828@qq.com>
2022-01-03 13:44:08 +0800

    add

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -13,19 +13,21 @@ import { NzFormTooltipIcon } from 'ng-zorro-antd/form';
   styleUrls: ['./calculator.component.scss'],
 })
 export class CalculatorComponent implements OnInit {
-  tabsetIndex = 0;
   yearModel = 20;
   rateModel = 'year';
   firstPaymentModel = 3;
   typeModel = 'A';
 
-  resultList = new Array(40).fill(true).map((v: any, index: number) => {
-    const y = (index + 1) * 5;
-    return {
-      label: `${y}年 （${y * 12}期）`,
-      value: y,
-    };
-  });
+  tabsetIndex = 0;
+  borrowTotal = 0;
+
+  data = {
+    total: 0,
+    interest: 0,
+    months: 0,
+  };
+
+  monthsList: string[] = [];
 
   yearList = new Array(6).fill(true).map((v: any, index: number) => {
     const y = (index + 1) * 5;
@@ -55,76 +57,71 @@ export class CalculatorComponent implements OnInit {
       };
     });
 
-  provinceChange(value: string): void {
-    // this.selectedCity = this.cityData[value][0];
-  }
-
-  title = 'calculator';
   validateForm!: FormGroup;
   captchaTooltipIcon: NzFormTooltipIcon = {
     type: 'info-circle',
     theme: 'twotone',
   };
 
-  submitForm(): void {
-    if (this.validateForm.valid) {
-      console.log('submit', this.validateForm.value);
-    } else {
-      let f = true;
-      Object.values(this.validateForm.controls).forEach((control) => {
-        if (control.invalid) {
-          control.markAsDirty();
-          control.updateValueAndValidity({ onlySelf: true });
-        } else {
-          f = false;
-        }
-      });
-
-      this.validateForm.validator;
-      console.log(f);
-
-      this.tabsetIndex++;
+  month() {
+    try {
+      return this.validateForm.value.return === 'A';
+    } catch (e) {
+      return false;
     }
-  }
-
-  updateConfirmValidator(): void {
-    /** wait for refresh value */
-    //@ts-ignore
-    Promise.resolve().then(() =>
-      //@ts-ignore
-      this.validateForm.controls.checkPassword.updateValueAndValidity()
-    );
-  }
-
-  confirmationValidator = (control: FormControl): { [s: string]: boolean } => {
-    if (!control.value) {
-      return { required: true };
-      //@ts-ignore
-    } else if (control.value !== this.validateForm.controls.password.value) {
-      return { confirm: true, error: true };
-    }
-    return {};
-  };
-
-  getCaptcha(e: MouseEvent): void {
-    e.preventDefault();
   }
 
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.validateForm = this.fb.group({
-      email: [null],
-      year: [null],
-      type: [null],
-      password: [null, [Validators.required]],
-      checkPassword: [null, [Validators.required, this.confirmationValidator]],
-      nickname: [null, [Validators.required]],
-      phoneNumberPrefix: ['+86'],
-      phoneNumber: [null, [Validators.required]],
-      website: [null, [Validators.required]],
-      website1: [null, [Validators.required]],
-      captcha: [null, [Validators.required]],
+      total: [null, [Validators.required]],
+      firstPayment: [null],
+      rateType: [null],
+      rate: [null, [Validators.required]],
+      time: [null],
+      return: [null],
     });
+  }
+
+  submitForm(): void {
+    if (this.validateForm.valid) {
+      console.log('submit', this.validateForm.value);
+      const time = this.validateForm.value.time;
+      const interest =
+        this.borrowTotal * (this.validateForm.value.rate / 100) * time;
+      this.data.total = interest + this.borrowTotal;
+      this.data.interest = interest;
+      // @ts-ignore
+
+      if (this.validateForm.value.return === 'A') {
+        this.data.months = (this.data.total / (12 * time)) * 10000;
+      } else {
+        this.monthsList = new Array(12 * time)
+          .fill(true)
+          .map((item: any, i: number) => {
+            return `第${i + 1}期，---`;
+          });
+      }
+
+      this.tabsetIndex++;
+    } else {
+      Object.values(this.validateForm.controls).forEach((control) => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
+  }
+
+  back() {
+    this.tabsetIndex--;
+  }
+
+  updateBorrowTotal(): void {
+    const total = this.validateForm.get('total')?.value;
+    const first = this.validateForm.get('firstPayment');
+    this.borrowTotal = total - total * (first?.value * 0.1);
   }
 }
